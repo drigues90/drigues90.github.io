2318012a90282fdfef3cd8fcffa207a27a0eaa46
Jonathan Jesus <drigues90@gmail.com>
2019-01-16 19:59:39 -0300

    fruit slicer
    primeiro commit

---
+++ fruitslicer/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1547679553,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/panelmenu-sheet0.png",
+		"images/paneltitulo-sheet0.png",
+		"images/btnmenu-sheet0.png",
+		"images/btnsettings-sheet0.png",
+		"images/spawner-sheet0.png",
+		"images/banana-sheet0.png",
+		"images/slash-sheet0.png",
+		"images/splash-sheet0.png",
+		"images/splash-sheet1.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
